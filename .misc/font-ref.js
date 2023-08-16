207b70ddebf044e574571b9ec28f043d9ea86f29
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-08-16 08:43:47 +0530

    chore: add font-refs

---
+++ .misc/font-ref.js
@@ -0,0 +1,11 @@
+// serif
+import "@fontsource/playfair-display/400.css";
+import "@fontsource/playfair-display/700.css";
+
+// sans
+import "@fontsource/space-grotesk/400.css";
+import "@fontsource/space-grotesk/700.css";
+
+// mono
+import "@fontsource/space-mono/400.css";
+import "@fontsource/space-mono/700.css";
