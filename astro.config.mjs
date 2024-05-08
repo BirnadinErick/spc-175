cf9716a7bf84947418b15806b43a7cb4f40ded27
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-05-08 12:57:43 +0200

    feat: add alpinejs integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import mdx from "@astrojs/mdx";
 import remarkGfm from "remark-gfm";
 import emoji from "remark-emoji";
 import remarkUnwrapImages from "remark-unwrap-images";
+import alpinejs from "@astrojs/alpinejs";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), mdx()],
-    experimental: {
-        assets: true,
-    },
-    markdown: {
-        remarkPlugins: [remarkGfm, emoji, remarkUnwrapImages],
-    },
+  integrations: [tailwind(), mdx(), alpinejs()],
+  experimental: {
+    assets: true
+  },
+  markdown: {
+    remarkPlugins: [remarkGfm, emoji, remarkUnwrapImages]
+  }
 });
