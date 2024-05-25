1fd74e081b5f11d87fa2643a8be8d0283335ec3a
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-05-25 21:53:21 +0200

    feat: add new navigation mockups and spawners

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -52,55 +52,55 @@ export default NAVIGATION_MENU_MANIFEST;
 export const NEW_NAVS = [
   {
     title: 'Alma Mater',
-    link: '#alma-mater',
+    link: 'alma-mater',
     children: [
-      { title: 'History of the College', link: '#history-of-the-college' },
-      { title: 'Our Spirituality', link: '#our-spirituality' },
-      { title: 'Founders', link: '#founders' },
-      { title: 'Motto, Vision & Mission', link: '#motto-vision-mission' },
-      { title: 'Coat of Arms', link: '#coat-of-arms' },
-      { title: 'College Anthem', link: '#college-anthem' },
-      { title: 'College Houses', link: '#college-houses' },
-      { title: 'Rectors', link: '#rectors' },
+      { title: 'History of the College', link: 'history-of-the-college' },
+      { title: 'Our Spirituality', link: 'our-spirituality' },
+      { title: 'Founders', link: 'founders' },
+      { title: 'Motto, Vision & Mission', link: 'motto-vision-mission' },
+      { title: 'Coat of Arms', link: 'coat-of-arms' },
+      { title: 'College Anthem', link: 'college-anthem' },
+      { title: 'College Houses', link: 'college-houses' },
+      { title: 'Rectors', link: 'rectors' },
     ]
   },
   {
     title: 'Administration',
-    link: '#administration',
+    link: 'administration',
     children: [
-      { title: 'Managing Committee', link: '#managing-committee' },
-      { title: 'Rectors', link: '#rectors' },
-      { title: 'Vice Rector', link: '#vice-rector' },
-      { title: 'Deputy Principals', link: '#deputy-principals' },
-      { title: 'Staff', link: '#staff' }
+      { title: 'Managing Committee', link: 'managing-committee' },
+      { title: 'Rectors', link: 'rectors' },
+      { title: 'Vice Rector', link: 'vice-rector' },
+      { title: 'Deputy Principals', link: 'deputy-principals' },
+      { title: 'Staff', link: 'staff' }
     ]
   },
   {
     title: 'Academics',
-    link: '#academic',
+    link: 'academics',
     children: [
-      { title: 'Overview', link: '#overview' },
-      { title: 'Achievements', link: '#achievements' },
-      { title: 'News', link: '#news' }
+      { title: 'Overview', link: 'overview' },
+      { title: 'Achievements', link: 'achievements' },
+      { title: 'News', link: 'news' }
     ]
   },
   {
     title: 'Co-Curriculum',
-    link: '#co-curriculum',
+    link: 'co-curriculum',
     children: [
-      { title: 'Clubs', link: '#clubs' },
-      { title: 'Sports', link: '#sports' }
+      { title: 'Clubs', link: 'clubs' },
+      { title: 'Sports', link: 'sports' }
     ]
   },
   {
     title: 'Students',
-    link: '#students',
+    link: 'students',
     children: [
-      { title: 'College Sections', link: '#college-sections' },
-      { title: 'Prefects', link: '#prefects' }
+      { title: 'College Sections', link: 'college-sections' },
+      { title: 'Prefects', link: 'prefects' }
     ]
   },
   { title: 'Patrician Publications', link: 'patrician-publications', children: [] },
-  { title: 'Facilities', link: '#facilities', children: [] },
-  { title: 'Gallery', link: '#gallery', children: [] }
+  { title: 'Facilities', link: 'facilities', children: [] },
+  { title: 'Gallery', link: 'gallery', children: [] }
 ]
