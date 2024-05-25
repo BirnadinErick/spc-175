35d6db4eb1b044364f47bd359ef896f8ff270c48
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-05-25 13:01:56 +0200

    feat: new mobile navigation

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -49,3 +49,56 @@ const NAVIGATION_MENU_MANIFEST = [
   },]
 
 export default NAVIGATION_MENU_MANIFEST;
+export const NEW_NAVS = [
+  {
+    title: 'Alma Mater',
+    link: '#alma-mater',
+    children: [
+      { title: 'History of the College', link: '#history-of-the-college' },
+      { title: 'Our Spirituality', link: '#our-spirituality' },
+      { title: 'Founders', link: '#founders' },
+      { title: 'Motto, Vision & Mission', link: '#motto-vision-mission' },
+      { title: 'Coat of Arms', link: '#coat-of-arms' },
+      { title: 'College Anthem', link: '#college-anthem' },
+      { title: 'College Houses', link: '#college-houses' },
+      { title: 'Rectors', link: '#rectors' },
+    ]
+  },
+  {
+    title: 'Administration',
+    link: '#administration',
+    children: [
+      { title: 'Managing Committee', link: '#managing-committee' },
+      { title: 'Rectors', link: '#rectors' },
+      { title: 'Vice Rector', link: '#vice-rector' },
+      { title: 'Deputy Principals', link: '#deputy-principals' },
+      { title: 'Staff', link: '#staff' }
+    ]
+  },
+  {
+    title: 'Academic',
+    link: '#academic',
+    children: [
+      { title: 'Achievements', link: '#achievements' },
+      { title: 'News', link: '#news' }
+    ]
+  },
+  {
+    title: 'Co-Curriculum',
+    link: '#co-curriculum',
+    children: [
+      { title: 'Clubs', link: '#clubs' },
+      { title: 'Sports', link: '#sports' }
+    ]
+  },
+  {
+    title: 'Students',
+    link: '#students',
+    children: [
+      { title: 'College Sections', link: '#college-sections' },
+      { title: 'Prefects', link: '#prefects' }
+    ]
+  },
+  { title: 'Facilities', link: '#facilities', children: [] },
+  { title: 'Gallery', link: '#gallery', children: [] }
+]
