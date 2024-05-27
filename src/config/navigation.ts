bb08b1c2c47a2a4f772d079e9a4fc95a7678f5f1
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-05-27 10:09:19 +0200

    feat: add desktop navmenu

--- src/config/navigation.ts
+++ src/config/navigation.ts
@@ -9,98 +9,99 @@ import socitiesIcon from "~/assets/nav-menu-socities.svg?raw";
 import alumniIcon from "~/assets/nav-menu-alumni.svg?raw";
 
 const NAVIGATION_MENU_MANIFEST = [
-  {
-    icon: homeIcon,
-    text: "Home",
-    link: "/",
-  },{
-    icon: blogIcon,
-    text: "Blogs",
-    link: "/patrician-publications",
-  },
-{
-    icon: aidIcon,
-    text: "Aid the SPC",
-    link: "/donate",
-  },{
-    icon: ourSpiritIcon,
-    text: "Our Spirituality",
-    link: "/our-spirituality",
-  },{
-    icon: academiaIcon,
-    text: "Academics",
-    link: "/academics",
-  },{
-    icon: sportsIcon,
-    text: "Sports",
-    link: "/sports",
-  },{
-    icon: facilitiesIcon,
-    text: "Facilities",
-    link: "/facilities",
-  },{
-    icon: socitiesIcon,
-    text: "Socities",
-    link: "/socities",
-  },{
-    icon: alumniIcon,
-    text: "Alumini",
-    link: "/alumini",
-  },]
+    {
+        icon: homeIcon,
+        text: "Home",
+        link: "/",
+    }, {
+        icon: blogIcon,
+        text: "Blogs",
+        link: "/patrician-publications",
+    },
+    {
+        icon: aidIcon,
+        text: "Aid the SPC",
+        link: "/donate",
+    }, {
+        icon: ourSpiritIcon,
+        text: "Our Spirituality",
+        link: "/our-spirituality",
+    }, {
+        icon: academiaIcon,
+        text: "Academics",
+        link: "/academics",
+    }, {
+        icon: sportsIcon,
+        text: "Sports",
+        link: "/sports",
+    }, {
+        icon: facilitiesIcon,
+        text: "Facilities",
+        link: "/facilities",
+    }, {
+        icon: socitiesIcon,
+        text: "Socities",
+        link: "/socities",
+    }, {
+        icon: alumniIcon,
+        text: "Alumini",
+        link: "/alumini",
+    },]
 
 export default NAVIGATION_MENU_MANIFEST;
 export const NEW_NAVS = [
-  {
-    title: 'Alma Mater',
-    link: 'alma-mater',
-    children: [
-      { title: 'History of the College', link: 'history-of-the-college' },
-      { title: 'Our Spirituality', link: 'our-spirituality' },
-      { title: 'Founders', link: 'founders' },
-      { title: 'Motto, Vision & Mission', link: 'motto-vision-mission' },
-      { title: 'Coat of Arms', link: 'coat-of-arms' },
-      { title: 'College Anthem', link: 'college-anthem' },
-      { title: 'College Houses', link: 'college-houses' },
-      { title: 'Rectors', link: 'rectors' },
-    ]
-  },
-  {
-    title: 'Administration',
-    link: 'administration',
-    children: [
-      { title: 'Managing Committee', link: 'managing-committee' },
-      { title: 'Rectors', link: 'rectors' },
-      { title: 'Vice Rector', link: 'vice-rector' },
-      { title: 'Deputy Principals', link: 'deputy-principals' },
-      { title: 'Staff', link: 'staff' }
-    ]
-  },
-  {
-    title: 'Academics',
-    link: 'academics',
-    children: [
-      { title: 'Overview', link: 'overview' },
-      { title: 'Achievements', link: 'achievements' },
-      { title: 'News', link: 'news' }
-    ]
-  },
-  {
-    title: 'Co-Curriculum',
-    link: 'co-curriculum',
-    children: [
-      { title: 'Clubs', link: 'clubs' },
-      { title: 'Sports', link: 'sports' }
-    ]
-  },
-  {
-    title: 'Students',
-    link: 'students',
-    children: [
-      { title: 'College Sections', link: 'college-sections' },
-      { title: 'Prefects', link: 'prefects' }
-    ]
-  },
-  { title: 'Patrician Publications', link: 'patrician-publications', children: [] },
-  { title: 'Facilities', link: 'facilities', children: [] },
-  { title: 'Gallery', link: 'gallery', children: [] }
+    {title: 'Home', link: '', children: []},
+    {
+        title: 'Alma Mater',
+        link: 'alma-mater',
+        children: [
+            {title: 'History of the College', link: 'history-of-the-college'},
+            {title: 'Our Spirituality', link: 'our-spirituality'},
+            {title: 'Founders', link: 'founders'},
+            {title: 'Motto, Vision & Mission', link: 'motto-vision-mission'},
+            {title: 'Coat of Arms', link: 'coat-of-arms'},
+            {title: 'College Anthem', link: 'college-anthem'},
+            {title: 'College Houses', link: 'college-houses'},
+            {title: 'Rectors', link: 'rectors'},
+        ]
+    },
+    {
+        title: 'Administration',
+        link: 'administration',
+        children: [
+            {title: 'Managing Committee', link: 'managing-committee'},
+            {title: 'Rectors', link: 'rectors'},
+            {title: 'Vice Rector', link: 'vice-rector'},
+            {title: 'Deputy Principals', link: 'deputy-principals'},
+            {title: 'Staff', link: 'staff'}
+        ]
+    },
+    {
+        title: 'Academics',
+        link: 'academics',
+        children: [
+            {title: 'Overview', link: 'overview'},
+            {title: 'Achievements', link: 'achievements'},
+            {title: 'News', link: 'news'}
+        ]
+    },
+    {
+        title: 'Co-Curriculum',
+        link: 'co-curriculum',
+        children: [
+            {title: 'Clubs', link: 'clubs'},
+            {title: 'Sports', link: 'sports'}
+        ]
+    },
+    {
+        title: 'Students',
+        link: 'students',
+        children: [
+            {title: 'College Sections', link: 'college-sections'},
+            {title: 'Prefects', link: 'prefects'}
+        ]
+    },
+    {title: 'Patrician Publications', link: 'patrician-publications', children: []},
+    {title: 'Facilities', link: 'facilities', children: []},
+    {title: 'Gallery', link: 'gallery', children: []}
 ]
