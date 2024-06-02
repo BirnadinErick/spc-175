7aad02ff004ee6de1cc25392e393f662ae8c87c6
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-06-02 17:40:51 +0200

    feat: add POST handler for projects

---
+++ api/v1/controllers/projects.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once MODELS . "projects.php";
+
+/**
+ * @throws Exception
+ *  ^- when the provided date can not be parsed
+ */
+function projects()
+{
+    if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        debug("new project request", __FILE__);
+        session_start();
+
+        $projects = new ProjectsModel();
+        $title = $_POST["project_title"];
+
+        // for different locale
+        $amount = $_POST["amount"];
+        $desc = $_POST["desc"];
+        $deadline = new DateTime($_POST["deadline"]); // TODO: add try-catch for invalid values
+
+        // standardize the data format in data store
+        $deadline = $deadline->format("Y-m-d");
+        $amount = (float)filter_var($amount, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+
+        debug($title . "," . $desc . "," . $amount . "," . $deadline, __FILE__);
+
+        $ok = $projects->addProject($title, $desc, $amount, $deadline);
+        if ($ok) {
+            debug("project " . $title . " was created", __FILE__);
+            http_response_code(201);
+
+            exit();
+        }
+
+        debug("failed to create project", __FILE__);
+        http_response_code(403);
+        session_write_close();
+        exit();
+
+    } elseif ($_SERVER["REQUEST_METHOD"] === "GET") {
+        echo "geting projects...";
+        return false;
+    } else {
+        return false;
+    }
+}
