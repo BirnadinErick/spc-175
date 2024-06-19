e0a82d27b231f8196a9dd07dafc2fd07da47b1d9
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-06-19 06:37:09 +0200

    feat: add update_content method

--- api/v1/models/contents.php
+++ api/v1/models/contents.php
@@ -10,6 +10,34 @@ class ContentsModel extends BaseModel
         $stmt->execute([$path, $uid, $updated_by, $data]);
     }
 
+    function update_content($path, $uid, $updated_by, $data) {
+        try {
+            // for now let's do this instead of using JOIN to check existence to
+            // keep the code compatible with both SQLite/MySQL/PostgresSQL
+            $checkSql = 'SELECT COUNT(*) FROM contents WHERE path = ?';
+            $checkStmt = $this->pdo->prepare($checkSql);
+            $checkStmt->execute([$path]);
+            $recordExists = $checkStmt->fetchColumn();
+
+            if ($recordExists) {
+                debug("content found, updating...", __FILE__);
+                $updateSql = 'UPDATE contents SET uid = ?, updated_by = ?, data = ?, updated_at = time("now") WHERE path = ?';
+                $updateStmt = $this->pdo->prepare($updateSql);
+                $updateStmt->execute([$uid, $updated_by, $data, $path]);
+
+                return 0;
+            } else {
+                debug("non-existent content", __FILE__);
+                return -1;
+            }
+        } catch (PDOException $e) {
+            debug($e->getMessage(), __FILE__);
+        }
+
+        return -1;
+    }
+
+
     function read_content(string $path) {
         $sql = 'SELECT * FROM contents WHERE path = ?';
         $stmt = $this->pdo->prepare($sql);
