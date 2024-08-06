b964f14c93169613575a13c57e2491b6fc43c3ad
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2024-08-06 10:53:46 +0200

    feat: add endpoint to project-comments

--- api/v1/controllers/Projects.php
+++ api/v1/controllers/Projects.php
@@ -3,11 +3,16 @@
 namespace tinyfuse\controllers;
 
 require_once MODELS . 'projects.php';
+require_once MODELS . 'users.php';
+require_once MODELS . 'comments.php';
 
 use BumpCore\EditorPhp\Helpers;
+use CommentsModel;
+use Faker\Extension\Helper;
 use JetBrains\PhpStorm\NoReturn;
 use ProjectsModel;
 use tinyfuse\lib\Constants;
+use tinyfuse\models\UsersModel;
 
 class Projects
 {
@@ -54,7 +59,32 @@ class Projects
         //HTTP Method: POST
         //parse the form and commit the data
         //return rendered view to be appended
-        http_response_code((int)Constants::Created);
+        $user = new UsersModel();
+        $comments = new CommentsModel();
+        $uid = $user->get_user_id($_SESSION['email']);
+        $comment = $_POST['comment'];
+
+        //add comment to database
+        $cid = $comments->addComment($pid, $comment, $uid);
+        if (gettype($cid) == "boolean"){
+            debug("failed to record comment for project $pid from user $uid", __FILE__);
+            http_response_code((int)Constants::InternalError);
+            exit(1);
+        }
+        //relate project with comment
+        $r = $comments->addProjectComment($pid, $cid);
+        if ($r === false){
+            debug("failed to relate comment $cid with project $pid", __FILE__);
+            http_response_code((int)Constants::InternalError);
+            exit(1);
+        }
+
+        // http_response_code((int)Constants::Created);
+        echo Helpers::renderNative(VIEWS.'project-new-comment-fragment.php', ['c'=>[
+            'fname' => $user->get_decorated_name($_SESSION['email']),
+            'lname' => '',
+            'comment' => $comment
+        ]]);
         session_write_close();
         exit(0);
     }
