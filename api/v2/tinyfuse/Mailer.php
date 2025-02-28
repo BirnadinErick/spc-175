d3acc31cd5f29f1e3be41bcaae2ee27ff509fc46
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2025-02-28 13:34:54 +0100

    add mailing facility

---
+++ api/v2/tinyfuse/Mailer.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace tinyfuse;
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+trait Mailer
+{
+    private function mail_html(string $to, string $to_name, string $body): bool
+    {
+        $mail = new PHPMailer(true);
+        try {
+            $mail->isSMTP();
+            $mail->Host = $this->state->get_env('MAIL_HOST') ?? 'localhost';
+            $mail->SMTPAuth = !$this->state->DEBUG;
+            $mail->Username = $this->state->get_env('MAIL_USERNAME') ?? 'root@localhost';
+            $mail->Password = $this->state->get_env('MAIL_PASSWORD') ?? 'spcmediaunit2023';
+            $mail->SMTPSecure = $this->state->DEBUG ? null : PHPMailer::ENCRYPTION_SMTPS;
+            $mail->Port = $this->state->get_env('MAIL_PORT') ?? 1025;
+
+            $mail->setFrom('info@spcjaffna.org', 'SPC Media Unit');
+            $mail->addAddress($to, $to_name);
+
+            $mail->isHTML(true);
+            $mail->Subject = 'Activate your account';
+            $mail->Body = $body;
+            $mail->AltBody = htmlspecialchars($body);
+
+            $mail->send();
+        } catch (Exception $e) {
+            if ($this->state->DEBUG) {
+                Utils::logDebug($e->getMessage());
+            } else {
+                Utils::logInfo('failed to send mail!');
+            }
+
+            return false;
+        }
+        return true;
+    }
+
+}
